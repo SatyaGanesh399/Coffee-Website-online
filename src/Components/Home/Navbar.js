c01723c1334b31acb565928cdcb42b4f788d2e35
Satya <satyaganesh399@gmail.com>
2022-09-27 18:15:26 +0530

    initial commit, navbar and carousel was completed

---
+++ src/Components/Home/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Home.css'
+import Logo from '../../Assests/Logo.png'
+export default function 
+() {
+  return (
+    <div className='navbar'>
+        <div className='navbar-left'>
+            <img src={Logo} alt="" className='company-logo'/>
+            <h4>cofféé</h4>
+        </div>
+        <div className='navbar-middle'>
+            <ul>
+                <li>Home</li>
+                <li>Products</li>
+                <li>About Us</li>
+                <li>Contact Us</li>
+            </ul>
+        </div>
+        <div className='navbar-right'>
+            <input 
+            type="search" 
+            name=""
+            className='search-box'
+            placeholder='Search' />
+            <button className='search-button'>Search</button>
+        </div>
+    </div>
+  )
+}
