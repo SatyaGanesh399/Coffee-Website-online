ddae8ab2702460bb103c2c2913f179b8a7d1262e
Satya <satyaganesh399@gmail.com>
2022-09-29 18:52:39 +0530

    Added footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Carousel from './Components/Home/Carousel';
 import OurProducts from './Components/OurProducts/OurProducts';
 import OnlineOrders from './Components/OnlineOrders/OnlineOrders';
 import Bevarages from './Components/Bevarages/Bevarages';
+import Footer from './Components/Footer/Footer';
 function App() {
   return (
     <div className="App">
@@ -14,6 +15,7 @@ function App() {
      <h3 className='online-orders'>Online Orders</h3> 
       <OnlineOrders />
       <Bevarages />
+      <Footer />
     </div>
   );
 }
