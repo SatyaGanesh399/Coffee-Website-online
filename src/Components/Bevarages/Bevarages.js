91241a1a79128938a830bd3161ecc65f7091dbf9
Satya <satyaganesh399@gmail.com>
2022-10-04 11:44:54 +0530

    added to cart class was implemented and cart was introduced

--- src/Components/Bevarages/Bevarages.js
+++ src/Components/Bevarages/Bevarages.js
@@ -1,26 +1,18 @@
 import React from "react";
 import orders from "./BevarageOrders";
 import './Bevarages'
+import ProductsBevarages from "./ProductsBevarages";
+
 function Bevarages() {
+
   const allOrders = orders.map((item) => (
-    <div key={item.id} className="order-card">
-      <img src={item.img} alt="" className="order-image" />
-      <h4 className="order-name">{item.name}</h4>
-      <div className="prices">
-        <p className="item-price">
-          Price :<strike>{item.price}</strike>
-        </p>
-        <p className="discounted-item-price">{item.discountedPrice}</p>
-      </div>
-      <div className="buttons">
-        <button type="button" className="buy-button">
-          Buy
-        </button>
-        <button type="button" className="add-button">
-          Add to cart
-        </button>
-      </div>
-    </div>
+    <ProductsBevarages
+    id = {item.id}
+    img = {item.img}
+    name = {item.name}
+    price = {item.price}
+    discountedPrice = {item.discountedPrice}
+     />
   ));
   return (
     <div>
